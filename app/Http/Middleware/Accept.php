49f47886791cc0fd084fc1b3d4bb3b8ae3104133
Ueki Chinos <uekichinos@users.noreply.github.com>
2021-01-18 05:27:05 +0000

    Apply fixes from StyleCI

--- app/Http/Middleware/Accept.php
+++ app/Http/Middleware/Accept.php
@@ -3,7 +3,6 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use App\User;
 use Illuminate\Support\Facades\Auth;
 
 class Accept
@@ -11,18 +10,18 @@ class Accept
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
         $user = Auth::user();
         $role = $user->getRoleNames()[0];
-        if(empty($user->accept_at) && $role == 'member') {
+        if (empty($user->accept_at) && $role == 'member') {
             return redirect()->route('accept.index');
-        }
-        else {
+        } else {
             return $next($request);
         }
     }
