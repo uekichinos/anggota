e796af728e79db78ba5242988a3b5349af080b73
Ueki Chinos <megatzulkhairi@gmail.com>
2021-01-30 13:30:29 +0800

    updated

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,16 @@
 <?php
-
+use App\Setting;
 use Illuminate\Support\Facades\Route;
 
 $relog = ['verify' => false, 'register' => false, 'reset' => false];
+
+$settings = Setting::where('param', 'LIKE', 'relog_register')->orWhere('param', 'LIKE', 'relog_reset')->get();
+if(count($settings) > 0) {
+    foreach($settings as $key => $setting) {
+        $tmp = explode('_', $setting->param);
+        $relog[$tmp[1]] = ($setting->value == 'yes' ? true : false);
+    }
+}
 
 /*
 |--------------------------------------------------------------------------
